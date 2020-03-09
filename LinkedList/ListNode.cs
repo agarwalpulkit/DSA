a39f5c2f6fbc5b8d2404f47cfe86aa847ccbf189
agarwalpulkit <pulkit.409.pa@gmail.com>
2020-03-09 15:56:06 +0530

    added linked list program

--- LinkedList/ListNode.cs
+++ LinkedList/ListNode.cs
@@ -1,19 +1,67 @@
-﻿namespace LinkedList
-{
-    public class ListNode
-    {
-        public int val;
-       
-        public ListNode next;
+﻿using System;
 
-        public ListNode(int x)
+namespace LinkedList
+{
+    
+    public class LinkList
+    {
+        public class ListNode
         {
-            val = x;
+            public int val;
+
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+
         }
 
-        public void Push(int data)
-        {
+        public ListNode head;
 
+        public void PushFront(int data)
+        {
+            if (head == null)
+                CreateHead(data);
+            else
+            {
+                ListNode node = new ListNode(data);
+                node.next = head;
+                head = node;
+            }
+        }
+
+        private void CreateHead(int data)
+        {
+            head = new ListNode(data);
+        }
+
+        public void PushBetween(ListNode prevNode, int data)
+        {
+            if (prevNode == null)
+                return;
+            ListNode node = new ListNode(data);
+            node.next = prevNode.next;
+            prevNode.next = node;
+        }
+
+        public void PushAfter(int data)
+        {
+            if (head == null)
+                CreateHead(data);
+            else
+            {
+                ListNode node = new ListNode(data);
+                node.next = null;
+                ListNode tmp = head;
+                while (tmp.next != null)
+                {
+                    tmp = tmp.next;
+                }
+
+                tmp.next = node;
+            }
         }
     }
 }
